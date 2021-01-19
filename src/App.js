96198d1f30fb139c45e884a643c22ebcfe7d50d3
Charles Buckley <charlesbuckley122@gmail.com>
2021-01-18 21:56:02 -0500

    init. displays basic prices

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import Map from './components/Map'
+var NumberFormat = require('react-number-format').default;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const values = document.querySelectorAll('.value');
+let sum = 0;
+
+
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cryptos: []
+    };
+  }
+
+  componentDidMount() {
+    // axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD')
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ADA,ETH,BNT,LOOM,DASH,XTZ&tsyms=USD')
+      .then(res => {
+        const cryptos = res.data;
+        console.log(cryptos.ADA.USD);
+        this.setState({ cryptos: cryptos });
+      })
+    
+    for (var i = 0; i < values.length; i++) {
+      console.log('test')
+      sum += parseInt(values[i]);
+    }
+    //console.log(sum)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {Object.keys(this.state.cryptos).map((key) => (
+          <div id="crypto-container">
+            <span className="left">{key}</span>
+
+            
+            <NumberFormat 
+            className="value"
+            value={this.state.cryptos[key].USD} 
+            displayType={'text'} 
+            decimalPrecision={2} 
+            thousandSeparator={true} 
+            prefix={'$'} 
+            />
+          </div>
+        ))}
+        <div id="crypto-container">
+          <span className="left">Total</span>
+
+
+          <span className="right">{this.sums}</span>
+        </div>
+        <Map />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Map.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import CHART from '../../src'
+
+
+class Map extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        // possible spans: day, week, month, 3month, year, all
+        return (
+        //     <CHART
+        //         id="crypto_chart"
+        //         height={300}
+        //         width={700}
+        //         fsym="ALIS"
+        //         tsym="JPY"
+        //         span="day"
+        //         borderColor="#61669F"
+        //     />
+        <h1>test</h1>
+        )
+       
+    }
+}
+
+export default Map;
